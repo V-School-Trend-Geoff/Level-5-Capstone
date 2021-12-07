06ccb2e81c0019872752603fea32143d6d235963
Geoff Davis <Davis.Geoff2@gmail.com>
2021-12-06 18:04:07 -0800

    5th_level_capstone/frontend_server: Got baseline working - Just get all records API -Just displays Vol, Book, Page, Rec Dt, and InsType

--- frontend_server/root/js/main.js
+++ frontend_server/root/js/main.js
@@ -5,78 +5,21 @@ import axios from 'axios';
 import { Form } from './InputForm';
 
 const App = () => {
-  const [bounties, setBounties] = useState([]);
+  const [instruments, setInstruments] = useState([]);
 
   useEffect(() => (async () => {
     try {
-      const { data } = await axios.get(`/bounties`);
-      setBounties(data);
+      const { data } = await axios.get(`/instrument`);
+      setInstruments(data);
     }
     catch (err) {
       console.log(err);
       alert(err);
     }
   })(), []);
-
-  const addBounty = async (bounty) => {
-    try {
-      const { data } = await axios.post(`/bounties`, bounty);
-      setBounties(prev => [...prev, data]);
-    }
-    catch (err) {
-      console.log(err);
-      alert(err);
-    }
-  }
-
-  // TODO Fix but that allows bad affilliation, doesn't catch the enum I set
-  // see https://github.com/Automattic/mongoose/issues/1974
-  const updateBounty = async (bounty) => {
-    try {
-      let { _id, ...body } = bounty;
-      const { data } = await axios.put(`/bounties/id/${_id}`, body);
-      setBounties(prev => prev.map(obj => obj._id === data._id ? data : obj));
-    }
-    catch (err) {
-      console.log(err);
-      alert(err);
-    }
-  }
-
-  const deleteBounty = async (_id) => {
-    try {
-      const { data } = await axios.delete(`/bounties/id/${_id}`);
-      setBounties(data);
-    }
-    catch (err) {
-      console.log(err);
-      alert(err);
-    }
-  }
-
-  const handleFilter = async (e) => {
-    try {
-      const { data } = await (e.target.value !== "All"
-        ? axios.get(`/bounties/search?Affiliation=${e.target.value}`)
-        : axios.get(`/bounties`))
-      setBounties(data)
-    }
-    catch (err) {
-      console.log(err);
-      alert(err);
-    }
-  }
-
+  
   return <>
-    <Form mode="Add" bounty={{ FirstName: "", LastName: "", Living: true, BountyAmount: "", Affiliation: "" }} handleSubmit={addBounty} /><br />
-    Filter by Affiliation <select onChange={handleFilter}>
-      <option value="All">- No Filter -</option>
-      <option value="Jedi">Jedi</option>
-      <option value="Sith">Sith</option>
-      <option value="Neutral">Neutral</option>
-    </select>
-    <hr />
-    {bounties.map((bounty) => <Form mode="Display" bounty={bounty} handleSubmit={updateBounty} handleDeleteClick={deleteBounty} key={bounty._id} />)}
+    {instruments.map((instrument) => <Form instrument={instrument} key={instrument._id} />)}
   </>
 
 }
